e8dfb83dc2c5b3f1b0db8a3bafeaad1dcdb62c66
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-09-08 15:51:44 -0600

    First commit.

---
+++ src/main/java/com/kevinjoramos/employeecrudapi/EmployeeCrudAPIApplication.java
@@ -0,0 +1,13 @@
+package com.kevinjoramos.employeecrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinjoramos/employeecrudapi/EmployeecrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinjoramos.employeecrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
