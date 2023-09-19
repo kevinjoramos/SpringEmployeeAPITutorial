07e165c28bb4c2b4e232abbb1c22ce06852da17b
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-09-18 22:55:57 -0600

    Completed rest of routes.

--- src/main/java/com/kevinjoramos/employeecrudapi/rest/EmployeeRestController.java
+++ src/main/java/com/kevinjoramos/employeecrudapi/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.kevinjoramos.employeecrudapi.dao.EmployeeDAO;
 import com.kevinjoramos.employeecrudapi.entity.Employee;
 import com.kevinjoramos.employeecrudapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,40 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        if (employeeService.findById(employeeId) == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 
 
